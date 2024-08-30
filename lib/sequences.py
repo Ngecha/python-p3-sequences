e299f56f248e9455f0129738b2387266f027c38e
Ngecha <ngechadennis@gmail.com>
2024-08-30 12:19:18 +0300

    assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length < 0:
+        print("Length must be a positive integer.")
+        return
+    
+    fibonacci_sequence = []
+    
+    for n in range(length):
+        if  n == 0:
+            fibonacci_sequence.append(0)
+        elif n == 1:
+                fibonacci_sequence.append(1)
+        else:
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+    
+    print(fibonacci_sequence)
+
+
+print_fibonacci(9)  
